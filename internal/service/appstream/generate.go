512b20683d53bdb542e09a60345a3a0bbe2c36c3
Graham Davison <gdavison@hashicorp.com>
2021-10-14 12:38:48 -0700

    Restores code generation checks

--- internal/service/appstream/generate.go
+++ internal/service/appstream/generate.go
@@ -1,3 +1,4 @@
+//go:generate go run -tags generate ../../generate/listpages/main.go -ListOps=DescribeFleets,DescribeImageBuilders,DescribeStacks -Export=yes
 //go:generate go run -tags generate ../../generate/tags/main.go -ListTags=yes -ServiceTagsMap=yes -UpdateTags=yes
 // ONLY generate directives and package declaration! Do not add anything else to this file.
 
---
+++ internal/service/autoscalingplans/generate.go
@@ -0,0 +1,4 @@
+//go:generate go run -tags generate ../../generate/listpages/main.go -ListOps=DescribeScalingPlans -Export=yes
+// ONLY generate directives and package declaration! Do not add anything else to this file.
+
+package autoscalingplans
